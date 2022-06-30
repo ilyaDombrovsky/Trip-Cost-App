fe901afe343275624d83058eacc9235747bb5ecd
Ilya Dambrouski <ilukha44@gmail.com>
2022-06-30 19:17:02 +0300

    base logic and base UI

---
+++ Trip Cost App/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Trop Cost App
+//
+//  Created by Ilya Dombrovsky on 30.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var consuptionFuelLabel: UILabel!
+    @IBOutlet weak var mileageTripLabel: UILabel!
+    @IBOutlet weak var costFuelLabel: UILabel!
+    @IBOutlet weak var tripCostLabel: UILabel!
+    @IBOutlet weak var mileageTripTF: UITextField!
+    @IBOutlet weak var costFuelTF: UITextField!
+    @IBOutlet weak var consuptionFuelSliderOutlet: UISlider! {
+        didSet {
+            consuptionFuelSliderOutlet.maximumValue = 20
+            consuptionFuelSliderOutlet.minimumValue = 0
+            consuptionFuelSliderOutlet.value = 0
+        }
+    }
+    
+    @IBAction func mileageTripTFAction(_ sender: UITextField) {
+    }
+    
+    @IBAction func costFuelTFAction(_ sender: UITextField) {
+    }
+    
+    @IBAction func consuptionFuelSlider(_ sender: UISlider) {
+        let consuptionFuel = Double(sender.value)
+        let stringFuel = String(format: "%.1f", consuptionFuel)
+        
+        consuptionFuelLabel.text! = "Расход топлива - \(stringFuel) литров"
+        
+        guard let consuptionFuelValueForTripCost = Double(String(format: "%.1f", consuptionFuel)) else { return }
+        guard let costFuel = Double(costFuelTF.text!) else { return }
+        guard var mileageTrip = Double(mileageTripTF.text!) else { return }
+        
+        mileageTrip /= 100
+        
+        let tripCost = (costFuel * consuptionFuelValueForTripCost * mileageTrip)
+        let tripCostString = String(format: "%.2f", tripCost)
+        
+        tripCostLabel.text! = "Стоимость поездки равна - \(tripCostString) рубля"
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+func logikApp () {
+    
+}
+
+
+
+
+
+
